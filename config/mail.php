6bd2bd1f2fbfc78a60e970aa5220e4b0fee7199f
fakebounce <benjamin.romanelli@neuf.fr>
2016-08-28 17:51:21 +0200

    gsmtp

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => "esgi.athleteec@gmail.com", 'name' => "Athleteec"],
+    'from' => ['address' => "benjamin.romanelli@neuf.fr", 'name' => "Athleteec"],
 
     /*
     |--------------------------------------------------------------------------
